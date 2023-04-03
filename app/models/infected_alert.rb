c34c7279581567a3f1839c291d99587a3a67bf89
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-03 15:30:58 -0300

    Implementa método de marcar como infectado no after save do infected alert

--- app/models/infected_alert.rb
+++ app/models/infected_alert.rb
@@ -1,4 +1,12 @@
 class InfectedAlert < ApplicationRecord
   belongs_to :user
   belongs_to :warned_by, class_name: 'User'
+
+  after_save :set_user_infected
+
+  def set_user_infected
+    return if self.user.infected || InfectedAlert.where(user_id: self.user_id).count <= 2
+    
+    self.user.update_attribute(:infected, true)
+  end
 end

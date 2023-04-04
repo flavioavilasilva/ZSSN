6e64aa2203e061f572b89a348b1ec26101cb2afb
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-04 19:20:06 -0300

    Ajusta validators para usuários infectados

--- app/models/validators/receiven_items_validator.rb
+++ app/models/validators/receiven_items_validator.rb
@@ -4,6 +4,13 @@ class Validators::ReceivenItemsValidator < ActiveModel::Validator
     return record.errors.add(:receiven_items, :is_empty) if record.receiven_items[:items].nil? || record.receiven_items[:items].empty?
     return record.errors.add(:receiven_items, :is_not_an_array) if record.receiven_items[:items].is_a?(Array) == false
     return record.errors.add(:receiven_items, :empty) if record.receiven_items[:items].empty?
-    return record.errors.add(:receiven_items, :equal_users) if record.receiven_items[:user_id] == record.given_items[:user_id] 
+    return record.errors.add(:receiven_items, :equal_users) if record.receiven_items[:user_id] == record.given_items[:user_id]
+    return record.errors.add(:receiven_items, :infected_user) if infected_user?(record)
   end
+
+  private
+    def infected_user?(record)
+      user = User.find(record.receiven_items[:user_id])
+      user.infected
+    end
 end

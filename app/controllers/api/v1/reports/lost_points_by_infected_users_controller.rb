ee473d513093ea2dae1b5cc94d5da8831e45bd53
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-04 11:38:12 -0300

    Implementa controllers de relatórios de acordo com a expectativa dos testes

---
+++ app/controllers/api/v1/reports/lost_points_by_infected_users_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Reports::LostPointsByInfectedUsersController < ApplicationController
+  # GET /api/v1/reports/lost_points_by_infected_users
+  def index
+    render json: { points: Reports::LostPointsByInfectedUser.points }
+  end
+end

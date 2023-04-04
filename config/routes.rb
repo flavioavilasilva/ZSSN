bb7b34f70594071bd37dc7b8e15947d5ece98c55
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-04 11:37:09 -0300

    Cria rotas dos relatórios

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       namespace :reports do
         resources :infected_users, only: [:index]
+        resources :not_infected_users, only: [:index]
+        resources :average_of_items_by_users, only: [:index]
+        resources :lost_points_by_infected_users, only: [:index]
       end
       resources :barters, only: [:create]
       resources :users do

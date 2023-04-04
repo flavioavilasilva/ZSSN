90e1a54c0cd50511177c41cd94e4b30914765ca7
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-04 18:36:07 -0300

    Ajusta implementação para fazer a validação da inclusão dos items

---
+++ app/models/validators/infected_users_validator.rb
@@ -0,0 +1,9 @@
+class Validators::InfectedUsersValidator < ActiveModel::Validator
+  def validate(record)
+    return record.errors.add(:user, :infected_user) if infected_user?(record)
+  end
+
+  def infected_user?(record)
+    record.user.infected
+  end
+end
---
+++ app/models/validators/name_items_validator.rb
@@ -0,0 +1,11 @@
+class Validators::NameItemsValidator < ActiveModel::Validator
+  def validate(record)
+    return record.errors.add(:name, :is_not_a_valid_item) if invalid_item?(record.name)
+  end
+
+  def invalid_item?(name)
+    return true if name.nil?
+    
+    Item::VALID_ITEMS.include?(name.to_sym) == false
+  end
+end

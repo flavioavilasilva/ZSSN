b00c8915b6e34cfa7a3c34f13b0d82bc0b600e48
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-05 18:42:12 -0300

    Refatora os validator de barter para ficar mais legivel

--- app/models/validators/barter/given_item_validator.rb
+++ app/models/validators/barter/given_item_validator.rb
@@ -2,23 +2,25 @@
 
 class Validators::Barter::GivenItemValidator < ActiveModel::Validator
   def validate(record)
-    return record.errors.add(:given_items, :is_nil) if record.given_items.nil?
-
-    if record.given_items[:items].nil? || record.given_items[:items].empty?
-      return record.errors.add(:given_items,
-                               :is_empty)
-    end
-    return record.errors.add(:given_items, :is_not_an_array) if record.given_items[:items].is_a?(Array) == false
-    return record.errors.add(:given_items, :empty) if record.given_items[:items].empty?
-
-    if record.given_items[:user_id] == record.receiven_items[:user_id]
-      return record.errors.add(:given_items,
-                               :equal_users)
-    end
+    return record.errors.add(:given_items, :blank_or_nil) if blank_or_nil?(record)
+    return record.errors.add(:given_items, :is_not_an_array) if not_an_array?(record)
+    return record.errors.add(:given_items, :same_users_id) if same_users_id?(record)
     return record.errors.add(:given_items, :infected_user) if infected_user?(record)
   end
 
   private
+
+  def same_users_id?(record)
+    record.given_items[:user_id] == record.receiven_items[:user_id]
+  end
+
+  def not_an_array?(record)
+    record.given_items[:items].is_a?(Array) == false
+  end
+
+  def blank_or_nil?(record)
+    record.given_items.nil? || record.given_items[:items].blank?
+  end
 
   def infected_user?(record)
     user = User.find(record.given_items[:user_id])

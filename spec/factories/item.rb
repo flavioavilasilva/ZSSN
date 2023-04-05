3121bf5fc27fd09ce3a0d2f52960107ac97374ba
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-05 01:33:47 -0300

    Cria factories das models existentes

---
+++ spec/factories/item.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :item do
+    name { "water" }
+    user { association :user }
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    name { "John" }
+    age  { 22 }
+  end
+end

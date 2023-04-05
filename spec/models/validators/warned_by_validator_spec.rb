6745418eddb82ff60e45cac40b4cdf9864772861
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-05 14:55:55 -0300

    Cria teste para validação de usuário que alerta não estar infectado

---
+++ spec/models/validators/warned_by_validator_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Validators::WarnedByValidator do
+  it 'is valid' do
+    user1 = create(:user)
+    user2 = create(:user)
+
+    expect(InfectedAlert.new(user: user1, warned_by: user2)).to be_valid
+  end
+  
+  context 'when warned user is infected' do
+    it 'is invalid' do
+      user1 = create(:user)
+      user2 = create(:user, infected: true)
+
+      expect(InfectedAlert.new(user: user1, warned_by: user2)).to_not be_valid
+    end
+  end
+
+  context 'when warned user is nil' do
+    it 'is invalid' do
+      user1 = create(:user)
+
+      expect(InfectedAlert.new(user: user1, warned_by: nil)).to_not be_valid
+    end
+  end
+end

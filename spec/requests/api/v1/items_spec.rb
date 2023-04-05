1d0b36e8a9199a8e4f30c231ee93a9d5e68d85ba
flavioavilasilva <flavio.avila.silva@outlook.com>
2023-04-05 12:29:42 -0300

    Ajusta testes de request para usar o factory_bot

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "/api/v1/items", type: :request do
-  
+RSpec.describe "/api/v1/users/1/items", type: :request do
   describe "GET /index" do
     it "renders a successful response" do
-      user = User.create!(name: "Elvis", age: 22)
-      Item.create!(name: "water", user: user)
+      user = create(:user)
+      create(:item, user: user)
 
       get api_v1_user_items_url(user), headers: {}, as: :json
       expect(response).to be_successful
@@ -14,8 +13,8 @@ RSpec.describe "/api/v1/items", type: :request do
 
   describe "GET /show" do
     it "renders a successful response" do
-      user = User.create!(name: "Elvis", age: 22)
-      item = Item.create!(name: "water", user: user)
+      user = create(:user)
+      item = create(:item, user: user)
 
       get api_v1_user_item_url(user, item), as: :json
       expect(response).to be_successful
@@ -25,7 +24,7 @@ RSpec.describe "/api/v1/items", type: :request do
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new Item" do
-        user = User.create!(name: "Elvis", age: 22)
+        user = create(:user)
 
         expect {
           post api_v1_user_items_url(user),
@@ -34,7 +33,7 @@ RSpec.describe "/api/v1/items", type: :request do
       end
 
       it "renders a JSON response with the new api_v1_item" do
-        user = User.create!(name: "Elvis", age: 22)
+        user = create(:user)
 
         post api_v1_user_items_url(user),
              params: { name: "water" }, headers: {}, as: :json
@@ -45,7 +44,7 @@ RSpec.describe "/api/v1/items", type: :request do
 
     context "with invalid parameters" do
       it "does not create a new Item" do
-        user = User.create!(name: "Elvis", age: 22)
+        user = create(:user)
 
         expect {
           post api_v1_user_items_url(user),
@@ -54,7 +53,7 @@ RSpec.describe "/api/v1/items", type: :request do
       end
 
       it "renders a JSON response with errors for the new api_v1_item" do
-        user = User.create!(name: "Elvis", age: 22)
+        user = create(:user)
         post api_v1_user_items_url(user),
              params: {}, headers: {}, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
@@ -65,8 +64,8 @@ RSpec.describe "/api/v1/items", type: :request do
 
   describe "DELETE /destroy" do
     it "destroys the requested api_v1_item" do
-      user = User.create!(name: "Elvis", age: 22)
-      item = Item.create!(name: "water", user: user)
+      user = create(:user)
+      item = create(:item, user: user)
 
       expect {
         delete api_v1_user_item_url(user, item), headers: {}, as: :json
